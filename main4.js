0d550ed3339b8e0324976073a1671bf650474305
andresisfx <andresrodriguezwg@gmail.com>
2025-06-17 21:52:18 -0500

    succes fetching Api

--- main4.js
+++ main4.js
@@ -14,3 +14,24 @@ colorbtn.addEventListener('click',function(){
     currentColor= (currentColor + 1)% colors.length;
     document.body.style.backgroundColor= colors[currentColor]
 })
+
+fetch('https://jsonplaceholder.typicode.com/users')
+ .then(response=>response.json())
+ .then(data=>{
+    const userDataTable=document.getElementById('userTable');
+
+    data.forEach(user => {
+        const row=document.createElement('tr');
+        row.innerHTML=`
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.number}</td>
+        `
+        userDataTable.appendChild(row)
+    });
+
+ })
+ .catch(error=>console.error("an error ocurred fetching API ",error.message));
+    
+
